6a84a7c8240ee6bc26ff3fd9d428bf39c423ecd1
Roger Vieira <rogervieira@ohio>
2018-08-16 07:59:20 -0300

    Initial folder structure

---
+++ src/main/java/br/com/ominilabs/agifileprocessor/AgifileprocessorApplication.java
@@ -0,0 +1,12 @@
+package br.com.ominilabs.agifileprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgifileprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgifileprocessorApplication.class, args);
+	}
+}
